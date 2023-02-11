acf8635440a736b29c950ed6ddc4b4ca5b797741
Susan <susankhadka708@gmail.com>
2023-02-11 07:11:27 +0530

    Badge Icon

--- lib/constant/common/NavBar/navbar.dart
+++ lib/constant/common/NavBar/navbar.dart
@@ -124,20 +124,25 @@ class NavBar extends StatelessWidget {
                 ? Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      badge.Badge(
-                        //  badgeColor: Colors.orange,
-                        badgeContent: const Text(
-                          '9',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        child: Container(
-                          margin: EdgeInsets.only(top: 6.h),
-                          child: Icon(
-                            Icons.star,
-                            size: kiconSize2,
+                      InkWell(
+                        onTap: () {
+                          routeTo("/leaderboard", context);
+                        },
+                        child: badge.Badge(
+                          //  badgeColor: Colors.orange,
+                          badgeContent: const Text(
+                            '9',
+                            style: TextStyle(color: Colors.white),
                           ),
+                          child: Container(
+                            margin: EdgeInsets.only(top: 6.h),
+                            child: Icon(
+                              Icons.star,
+                              size: kiconSize2,
+                            ),
+                          ),
+                          // showBadge: showBadge ? true : false,
                         ),
-                        // showBadge: showBadge ? true : false,
                       ),
                       Padding(
                         padding: const EdgeInsets.all(kPadding1),
